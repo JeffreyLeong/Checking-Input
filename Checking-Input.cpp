7b3f5b256af3ce7d957e6cd19bd6869b30efbe2e
dollarortwo <jleong001@gmail.com>
2022-03-26 10:30:12 -0700

    added Function Declarations and Functions Definitions

---
+++ Checking-Input.cpp
@@ -0,0 +1,39 @@
+// Program to demostrate the functions new_line and get_input.
+#include <iostream>
+
+void new_line();
+// Discards all the input remaining ont he current input line. Also discards the '\n at the end of the line. This version works only for input from the keyboard.
+
+void get_int(int& number);
+// Postcondition: The variable has been give a vlaue that the user approves of.
+
+int main()
+{
+
+}
+
+void new_line()
+{
+    using namespace std;
+    char symbol;
+    do
+    {
+        cin.get(symbol);
+    } while (symbol != '\n');
+    
+}
+
+void get_int(int& number)
+{
+    using namespace std;
+    char ans;
+    do
+    {
+        cout << "Enter input number: ";
+        cin >> number;
+        cout << "You entered " << number << ". Is that correct? (yes/no): ";
+        cin >> ans;
+        new_line();
+    } while ((ans != 'Y') && (ans != 'y'));
+    
+}
